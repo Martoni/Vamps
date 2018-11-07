082cf51771ae61f0c8357a15cd187b3315df59f5
Fabien Marteau <fabien.marteau@armadeus.com>
2018-11-07 23:02:15 +0100

    begin tests of fetch and decode

--- src/main/scala/vamps/Vamps.scala
+++ src/main/scala/vamps/Vamps.scala
@@ -5,6 +5,7 @@ package vamps
 
 import chisel3._
 
+
 class Vamps extends Module {
   val io = IO(new Bundle {
     /* instructions */
@@ -18,9 +19,59 @@ class Vamps extends Module {
 
     val wr = Output(Bool())
     val rd = Output(Bool())
+
+    val debugIFID = Output(UInt(32.W))
+    val debugrs1 = Output(UInt(32.W))
+    val debugrs2 = Output(UInt(32.W))
+    val debugrs1num = Output(UInt(5.W))
+    val debugrs2num = Output(UInt(5.W))
+
   })
 
-  val LUI = "b0110111".U
+  val INIT_ADDR = 0
+
+  val PCREG = RegInit(INIT_ADDR.U(32.W))
+  io.iaddr := PCREG
+
+  /* OPCODE */
+  val LUI  = "b0110111".U
+  val LOAD = "b0000011".U
+  val CTRL = "b1110011".U
+  /* FUNC3 */
+  val LB  = "b000".U
+  val LH  = "b001".U
+  val LW  = "b010".U
+  val LD  = "b011".U
+  val LBU = "b100".U
+  val LHU = "b101".U
+  val LWU = "b110".U
+
+  /* registers file */
+  val regfile = RegInit(VecInit(Seq.fill(31)(0.U(32.W))))
+
+  /* fetch */
+  val IFID = RegNext(io.idata)
+  io.debugIFID := IFID
+
+  /* instruction decode */
+  val rs1 = RegInit(0.U(32))
+  val rs2 = RegInit(0.U(32))
+  val opcode = RegInit(0.U(32))
+  val rs1num = Wire(UInt(5.W))
+  val rs2num = Wire(UInt(5.W))
+  io.debugrs1num := rs1num
+  io.debugrs2num := rs2num
+
+  rs1num := IFID(19, 15)
+  rs2num := IFID(24, 20)
+
+  rs1 := Mux(rs1num =/= 0.U, regfile(rs1num - 1.U), 0.U)
+  rs2 := Mux(rs2num =/= 0.U, regfile(rs2num - 1.U), 0.U)
+
+  opcode := IFID(6,0)
+
+  io.debugrs1 := rs1
+  io.debugrs2 := rs2
 
   /* tester be quiet */
   io.wr := RegInit(false.B)
---
+++ src/test/scala/vamps/VampsUnitTest.scala
@@ -0,0 +1,97 @@
+// See README.md for license details.
+
+package vamps
+
+import java.io.File
+
+import chisel3.iotesters
+import chisel3.iotesters.{ChiselFlatSpec, Driver, PeekPokeTester}
+
+class VampsUnitTester(c: Vamps) extends PeekPokeTester(c) {
+  val LOAD = "0000011"
+  val rsnum = "00001"
+  val data = "01"*10
+
+  poke(c.io.idata, Integer.parseInt(data + rsnum + LOAD, 2))
+  step(10)
+}
+
+/**
+  * This is a trivial example of how to run this Specification
+  * From within sbt use:
+  * {{{
+  * testOnly example.test.VampsTester
+  * }}}
+  * From a terminal shell use:
+  * {{{
+  * sbt 'testOnly example.test.VampsTester'
+  * }}}
+  */
+class VampsTester extends ChiselFlatSpec {
+  // Disable this until we fix isCommandAvailable to swallow stderr along with stdout
+  private val backendNames = if(false && firrtl.FileUtils.isCommandAvailable(Seq("verilator", "--version"))) {
+    Array("firrtl", "verilator")
+  }
+  else {
+    Array("firrtl")
+  }
+  for ( backendName <- backendNames ) {
+    "Vamps" should s"calculate proper greatest common denominator (with $backendName)" in {
+      Driver(() => new Vamps, backendName) {
+        c => new VampsUnitTester(c)
+      } should be (true)
+    }
+  }
+
+  "Basic test using Driver.execute" should "be used as an alternative way to run specification" in {
+    iotesters.Driver.execute(Array(), () => new Vamps) {
+      c => new VampsUnitTester(c)
+    } should be (true)
+  }
+
+  "using --backend-name verilator" should "be an alternative way to run using verilator" in {
+    if(backendNames.contains("verilator")) {
+      iotesters.Driver.execute(Array("--backend-name", "verilator"), () => new Vamps) {
+        c => new VampsUnitTester(c)
+      } should be(true)
+    }
+  }
+
+  "running with --is-verbose" should "show more about what's going on in your tester" in {
+    iotesters.Driver.execute(Array("--is-verbose"), () => new Vamps) {
+      c => new VampsUnitTester(c)
+    } should be(true)
+  }
+
+  /**
+    * By default verilator backend produces vcd file, and firrtl and treadle backends do not.
+    * Following examples show you how to turn on vcd for firrtl and treadle and how to turn it off for verilator
+    */
+
+  "running with --generate-vcd-output on" should "create a vcd file from your test" in {
+    iotesters.Driver.execute(
+      Array("--generate-vcd-output", "on", "--target-dir", "test_run_dir/make_a_vcd", "--top-name", "make_a_vcd"),
+      () => new Vamps
+    ) {
+
+      c => new VampsUnitTester(c)
+    } should be(true)
+
+    new File("test_run_dir/make_a_vcd/make_a_vcd.vcd").exists should be (true)
+  }
+
+  "running with --generate-vcd-output off" should "not create a vcd file from your test" in {
+    iotesters.Driver.execute(
+      Array("--generate-vcd-output", "off", "--target-dir", "test_run_dir/make_no_vcd", "--top-name", "make_no_vcd",
+      "--backend-name", "verilator"),
+      () => new Vamps
+    ) {
+
+      c => new VampsUnitTester(c)
+    } should be(true)
+
+    new File("test_run_dir/make_no_vcd/make_a_vcd.vcd").exists should be (false)
+
+  }
+
+}
